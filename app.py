240896b91fdcf82bdcaebe3a72a18a0296ed470e
FlashCoder2 <69975162+FlashCoder2@users.noreply.github.com>
2021-08-05 20:12:05 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+from classifier import get_prediction
+
+app = Flask(___name___)
+
+@app.route("/predict-alphabet", methods = ["POST"])
+
+def predict_data():
+    image = request.files.get("alphabet")
+
+    prediction = get_prediction(image)
+    
+    return jsonify({
+        "prediction" : prediction
+    }), 200
